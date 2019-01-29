6c7853b2450159b074f27c3c3ba6abf578f4fb8e
ol-rost <47034337+ol-rost@users.noreply.github.com>
2019-01-29 22:47:51 +0300

    Update DZ_7.py

--- DZ_7.py
+++ DZ_7.py
@@ -1,18 +1,28 @@
-#TODO1
-
 operation = input("Введите необходимую операцию (сложение - '+',вычитание - '-', деление - '/',  умножение - '*': ", )
-number_1 = int(input("Введите первый операнд: ", ))
-number_2 = int(input("Введите  второй операнд: ", ))
+try:
+  number_1 = int(input("Введите первый операнд: ", ))
+  number_2 = int(input("Введите  второй операнд: ", ))
 
-if operation == "+":
-  result = number_1 + number_2
-elif operation == "-":
-  result = number_1 - number_2
-elif operation == "/":
-  result = number_1 / number_2
-elif operation == "*":
-  result = number_1 * number_2
-else:
-  print("Такой операции не существует, начните программу заново")
 
-print(result)
+
+  if operation == "+":
+    result = number_1 + number_2
+  elif operation == "-":
+    result = number_1 - number_2
+  elif operation == "/":
+    result = number_1 / number_2
+  elif operation == "*":
+    result = number_1 * number_2
+  else:
+    print("Такой операции не существует, начните программу заново")
+
+  print(result)
+
+except ZeroDivisionError:
+  print("Произошла ошибка деления на ноль")
+
+except ValueError:
+  print("Произошла ошибка значений, в тип 'int' попытались присвоить другое значение")
+
+except Exception:
+  print("Произошла ошибка")
